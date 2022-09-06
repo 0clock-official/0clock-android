7b609eee437424ae2378f687080269b3b672e0f9
hanna2100 <devhanna91@gmail.com>
2022-09-06 17:22:44 +0900

    닉네임 정규식검사

--- app/src/main/java/com/xyz/oclock/ui/signup/nickname/SignUpNickNameViewModel.kt
+++ app/src/main/java/com/xyz/oclock/ui/signup/nickname/SignUpNickNameViewModel.kt
@@ -6,12 +6,16 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.skydoves.bindables.BindingViewModel
 import com.skydoves.bindables.bindingProperty
+import com.xyz.oclock.R
+import com.xyz.oclock.common.utils.ResourceProvider
 import com.xyz.oclock.ui.signup.SignUpViewPagerFragmentListener
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.launch
+import java.util.regex.Pattern
 
 class SignUpNickNameViewModel @AssistedInject constructor(
+    private val resourceProvider: ResourceProvider,
     @Assisted private val listener: SignUpViewPagerFragmentListener
 ): BindingViewModel() {
 
@@ -23,13 +27,40 @@ class SignUpNickNameViewModel @AssistedInject constructor(
         }
 
     @get: Bindable
-    var inputNickname by bindingProperty("")
+    var inputNickname: String = ""
+        set(value) {
+            field = value
+            checkNicknameFormat(value)
+            notifyPropertyChanged(::nicknameFormatError)
+        }
 
     @get: Bindable
-    var nicknameFormatError: String = ""
+    var nicknameFormatError by bindingProperty("")
 
     fun checkDuplicateNickname() = viewModelScope.launch {
-        listener.moveToNextStep()
+        nicknameFormatError = resourceProvider.getString(R.string.error_nickname_format4)
+//        listener.moveToNextStep()
+    }
+
+
+    private fun checkNicknameFormat(nickname: String) {
+        var nicknamePattern = "^[A-Za-z가-힣]{1,10}\$"
+        var pattern = Pattern.compile(nicknamePattern)
+        if (pattern.matcher(nickname).matches()) {
+            nicknameFormatError = ""
+        } else {
+            if (nickname.isEmpty() || nickname.length > 10) {
+                nicknameFormatError = resourceProvider.getString(R.string.error_nickname_format1)
+                return
+            }
+            nicknamePattern = "([0-9])"
+            pattern = Pattern.compile(nicknamePattern)
+            if (pattern.matcher(nickname).find()) {
+                nicknameFormatError = resourceProvider.getString(R.string.error_nickname_format2)
+            } else {
+                nicknameFormatError = resourceProvider.getString(R.string.error_nickname_format3)
+            }
+        }
     }
 
     @dagger.assisted.AssistedFactory
